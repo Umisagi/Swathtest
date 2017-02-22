f7faa9a2f9d8a276ffc741472b73ec35736f6de9
Umisagi <zenchiter@gmail.com>
2017-02-22 16:33:48 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,15 +1,21 @@
 <?php
-$access_token = '1XvzTYDBQ/aCf6BZ/0JtbhHzdeAJzgCbTehPsx+n+eyXBiyPJw9tHCd6ktqqUnj0vorYI39lJux/oGfscxvSS72Gln1UO8kUfL1+nthpyj4fZPSCBMkRup7+5pajYZ9Js/Jf16s2vjtIzHEuME6OSwdB04t89/1O/w1cDnyilFU=';
-
-$url = 'https://api.line.me/v1/oauth/verify';
-
-$headers = array('Authorization: Bearer ' . $access_token);
-
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-$result = curl_exec($ch);
-curl_close($ch);
-
-echo $result;
+ 
+    function swath($input_text) 
+    {
+        $input_filename= tempnam("/tmp", "swath_");
+        $output_filename= tempnam("/tmp", "swath_");
+        $input_text_tis620 = iconv("UTF-8", "TIS-620", $input_text);
+        file_put_contents($input_filename, $input_text_tis620);
+        system("/usr/bin/swath < $input_filename > $output_filename");
+        $raw = file_get_contents($output_filename);
+        $raw_utf8 = iconv("TIS-620", "UTF-8", $raw);
+        unlink($input_filename);
+        unlink($output_filename);
+        return preg_split('/\|/', $raw_utf8);
+    }
+ 
+    if($_REQUEST['input_text']) {
+        $output = swath("สวัสดีครับ");
+        print implode(" ", $output);
+    }
+?>
