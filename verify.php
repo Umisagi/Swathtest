c34a80e0681d464fe28a0c8245fc20333d737dfe
Umisagi <zenchiter@gmail.com>
2017-02-22 16:34:58 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -6,7 +6,7 @@
         $output_filename= tempnam("/tmp", "swath_");
         $input_text_tis620 = iconv("UTF-8", "TIS-620", $input_text);
         file_put_contents($input_filename, $input_text_tis620);
-        system("/usr/bin/swath < $input_filename > $output_filename");
+        system("/swath < $input_filename > $output_filename");
         $raw = file_get_contents($output_filename);
         $raw_utf8 = iconv("TIS-620", "UTF-8", $raw);
         unlink($input_filename);
@@ -15,7 +15,7 @@
     }
  
     if($_REQUEST['input_text']) {
-        $output = swath("สวัสดีครับ");
+        $output = swath($_REQUEST['input_text']);
         print implode(" ", $output);
     }
 ?>
